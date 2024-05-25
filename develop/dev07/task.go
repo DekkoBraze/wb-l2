e38510c5ab476036c31e9c2a43c261f6da7acfab
DekkoBraze <greende556@gmail.com>
2024-05-26 00:54:04 +0300

    dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -34,5 +39,66 @@ fmt.Printf(“fone after %v”, time.Since(start))
 */
 
 func main() {
+	// Определение
+	var or func(channels ...<-chan interface{}) <-chan interface{}
 
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		// Базовые случаи - если нет аргументов выдаем nil, если один - возвращаем его
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		// Канал, по которому будем сообщать другим рекурсивно созданным горутинам о завершении работы
+		orDone := make(chan interface{})
+
+		go func() {
+			defer close(orDone)
+
+			switch len(channels) {
+			// При n=2 прописываем case вручную
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			// В случае n>2, рекурсивно создаем новую горутину, куда передаем необработанные аргументы
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+
+			}
+
+		}()
+		// По завершении передаем закрытый orDone
+		return orDone
+	}
+
+	// Горутина для тестирования
+	sig := func(after time.Duration) <- chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+	}()
+	return c
+	}
+	
+	start := time.Now()
+	// Тест
+	<-or (
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	
+	fmt.Printf("fone after %v\n", time.Since(start))
 }
