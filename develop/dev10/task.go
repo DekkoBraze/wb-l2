838e43140c54fd86720f7a2329a6ed9abd2cb1d8
DekkoBraze <greende556@gmail.com>
2024-05-27 02:49:08 +0300

    dev10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -16,5 +28,62 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 */
 
 func main() {
+	timeoutFlag := flag.Uint("timeout", 10, "указать таймаут подключения")
+	flag.Parse()
 
+	if flag.NArg() < 2 {
+		panic(errors.New("ip или порт не указаны"))
+	}
+
+	ip := flag.Arg(0)
+	port := flag.Arg(1)
+	timeout := time.Duration(*timeoutFlag) * time.Second
+
+	// Коннектимся к указанному сокету с таймаутом
+	conn, err := net.DialTimeout("tcp", ip+":"+port, timeout)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	readerForSend := bufio.NewReader(os.Stdin)
+	readerForReceive := bufio.NewReader(conn)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// Запись в указанный сокет
+	go func() {
+		for {
+			input, err := readerForSend.ReadString('\n')
+			// Если 
+			if err != nil {
+				if err == io.EOF {
+					conn.Close()
+					wg.Done()
+					return
+				}
+			}
+			_, err = conn.Write([]byte(input))
+			if err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	// Вывод сообщений из указанного сокета
+	go func() {
+		for {
+			message, err := readerForReceive.ReadString('\n')
+			if err != nil {
+				fmt.Println("Соединение закрыто.")
+				conn.Close()
+				wg.Done()
+				return
+			}
+			fmt.Print(message)
+		}
+	}()
+
+	wg.Wait()
 }
