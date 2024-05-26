cf50768b414ae127f647f9f390f70718a6fad44c
DekkoBraze <greende556@gmail.com>
2024-05-26 18:17:15 +0300

    dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +18,38 @@ package main
 */
 
 func main() {
+	flag.Parse()
 
+	if flag.NArg() < 1 {
+		panic(errors.New("ссылка на ресурс не указана"))
+	}
+
+	link := flag.Arg(0)
+	resp, err := http.Get(link)
+	if err != nil {
+		panic(err)
+	}
+
+	// Создаем папку с именем сайта
+	splittedLink := strings.Split(link, "//")
+	dirName := strings.Split(splittedLink[1], "/")[0]
+	err = os.Mkdir(dirName, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	// Сохраняем body из респонса
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Создаем файл index.html в новой папке и сохраняем туда body
+	f, err := os.Create(dirName + "/index.html")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString(string(bytes))
 }
