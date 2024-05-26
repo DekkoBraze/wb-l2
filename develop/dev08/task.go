e1d6bf0729eff1676f0ce683a8340535b25096d9
DekkoBraze <greende556@gmail.com>
2024-05-26 12:24:47 +0300

    dev08

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -14,6 +25,107 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func cd(folder string, currentDir string) string{
+	if err := os.Chdir(folder); err != nil {
+		fmt.Println(err)
+	}
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return currentDir
+}
 
+func pwd() {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(currentDir)
+}
+
+func echo(arg string) {
+	fmt.Println(arg)
+}
+
+func kill(pid int) {
+	syscall.Kill(pid, syscall.SIGTERM)
+}
+
+func ps() {
+	out, err := exec.Command("/bin/sh", "-c", "ps").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	strOut := string(out)
+	fmt.Println(strOut[:len(strOut)-1])
+}
+
+func nc(ip string, port string, message string) {
+	tcpServer, err := net.ResolveTCPAddr("tcp", ip+":"+port)
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, tcpServer)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	// Получаем текущую директорию и отображаем её
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Добро пожаловать в shell! Чтобы выйти, введите /q.")
+	fmt.Printf(currentDir + "$ ")
+	for {
+		input, _ := reader.ReadString('\n')
+		if input == "/q\n" {
+			break
+		}
+		splittedInput := strings.Split(input, " ")
+		switch splittedInput[0] {
+		case "cd":
+			// Удаляем знак переноса строки из аргумента
+			arg := splittedInput[1][:len(splittedInput[1])-1]
+			currentDir = cd(arg, currentDir)
+		case "pwd\n":
+			pwd()
+		case "echo":
+			arg := splittedInput[1][:len(splittedInput[1])-1]
+			echo(arg)
+		case "kill":
+			arg, err := strconv.Atoi(splittedInput[1])
+			if err != nil {
+				fmt.Println("Некорректные аргументы команды.")
+				break
+			}
+			kill(arg)
+		case "ps\n":
+			ps()
+		case "nc":
+			if len(splittedInput) != 4 {
+				fmt.Println("Некорректные аргументы команды.")
+				break
+			}
+			nc(splittedInput[1], splittedInput[2], splittedInput[3][:len(splittedInput[3])-1])
+		default:
+			if len(splittedInput) != 2 {
+				fmt.Println("Некорректные аргументы команды.")
+				break
+			}
+			fmt.Println("Неизвестная команда.")
+		}
+		fmt.Printf(currentDir + "$ ")
+	}
+	fmt.Println("До встречи!")
 }
--- develop/dev08/task_test.go
+++ develop/dev08/task_test.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"net"
+	"os"
+	"testing"
+)
+
+func TestCd(t *testing.T) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Mkdir("testFolder", 0777)
+	rightPath := currentDir + "/testFolder"
+	defer os.RemoveAll(rightPath)
+	cd("testFolder", currentDir)
+	currentDir, err = os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if currentDir != rightPath {
+		t.Errorf("handler returned unexpected message: got %v want %v",
+			currentDir, rightPath)
+	}
+}
+
+func TestNc(t *testing.T) {
+	listen, err := net.Listen("tcp", "localhost"+":"+"8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listen.Close()
+
+	ch := make(chan bool)
+	go func() {
+		for {
+			_, err := listen.Accept()
+			if err != nil {
+				ch <- false
+			}
+			ch <- true
+			break
+		}
+	}()
+	
+	nc("localhost", "8080", "Hello!")
+	ans := <-ch
+	if !ans {
+		t.Error("Ошибка получения сообщения")
+	}
+}
